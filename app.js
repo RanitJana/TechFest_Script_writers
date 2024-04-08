02ad9320b11c5966a3fe7d789f8648dcf83c6c95
Ranit Jana <ranitjana100@gmail.com>
2024-04-08 17:13:50 +0530

    commited

--- app.js
+++ app.js
@@ -20,3 +20,52 @@ book_add_btn.addEventListener('click', () => {
     }, 2000);
     localStorage.setItem("books", JSON.stringify(bookData));
 });
+
+
+let userData = [];
+let customer_name = document.querySelector("#customer_name");
+let bookName = document.querySelector("#bookName");
+let count = document.querySelector('#count');
+let msg = document.querySelector('.msgUser');
+console.log(customer_name.value);
+console.log(bookName.value);
+console.log(count.value);
+
+let userBtn = document.querySelector('.userFind');
+userBtn.addEventListener('click', () => {
+
+
+
+    let idx = bookData.findIndex(val => val.name == bookName);
+    if (idx == -1) {
+        msg.innerHTML = `${bookName.value} is not available in our library.`;
+        return;
+    }
+    if (bookData[idx].count == 0) {
+        msg.innerHTML = `${bookName.value} is not available currently.`;
+        return;
+    }
+    if (bookData[idx].count > count.value) {
+        msg.innerHTML = `Total${count.value} books of ${bookName.value} is available currently.`;
+        return;
+    }
+    let userInfo = userData.findIndex(val => customer_name.value == val.value);
+    if (userInfo != -1) {
+        let bookIdx = userData[userInfo].books.findIndex(obj => bookName == obj.bookName);
+        if (bookIdx == -1) {
+            userData[userInfo].books.push({ bookName: bookName.value, count: count.value });
+        }
+        else {
+            userData[userInfo].books[bookIdx] = +userData[userInfo].books[bookIdx] + +count.value;
+        }
+    }
+    else {
+        let data = {
+            name: customer_name.value,
+            books: [{ bookName: bookName.value, count: count.value }]
+        }
+        userData.push(data);
+        localStorage.setItem('user', JSON.stringify(userData));
+    }
+})
+
